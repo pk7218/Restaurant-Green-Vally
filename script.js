4af6d6a8d865ec9852bff2838617b2ee97d099b8
Pramod Kumbhar <kumbharpramod30@gmail.com>
2021-12-01 18:54:23 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// const formPop = document.getElementById('myForm');
+// const btn = document.getElementById('btnbook');
+
+// btn.onclick = function (e) {
+//     // formPop.classList.toggle("active");
+//     console.log('ok');
+// }
+function openForm() {
+    // document.getElementById("ovalshape").style.display="none";
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    // document.getElementById("ovalshape").style.display="block";
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openForm1() {
+    document.getElementById("ovalshape").style.display="none";
+    // document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm1() {
+    document.getElementById("ovalshape").style.display="block";
+    // document.getElementById("myForm").style.display = "none";
+  }
+  function accept()
+{
+   alert("Table reserved.!");
+}  
